2abf901f46fe501419e4301b8f099766f94163b5
Willem de Groot <willem@byte.nl>
2014-12-10 15:57:40 -0200

    Initial release

---
+++ debian/libapache2-mod-rpaf-gnif.default.ex
@@ -0,0 +1,10 @@
+# Defaults for libapache2-mod-rpaf-gnif initscript
+# sourced by /etc/init.d/libapache2-mod-rpaf-gnif
+# installed at /etc/default/libapache2-mod-rpaf-gnif by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libapache2-mod-rpaf-gnif):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libapache2-mod-rpaf-gnif" command="/usr/bin/libapache2-mod-rpaf-gnif"
